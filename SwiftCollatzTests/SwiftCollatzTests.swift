776808c8724fb85580a9ceeeea2f209138a62b09
178inaba <178inaba@users.noreply.github.com>
2015-10-18 18:57:05 +0900

    first commit

---
+++ SwiftCollatzTests/SwiftCollatzTests.swift
@@ -0,0 +1,36 @@
+//
+//  SwiftCollatzTests.swift
+//  SwiftCollatzTests
+//
+//  Created by 178inaba on 2015/10/18.
+//  Copyright © 2015年 178inaba. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftCollatz
+
+class SwiftCollatzTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
